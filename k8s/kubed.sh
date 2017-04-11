86cbdb21a32e21dc3c3c4beff30bf9ce3cfdc4ed
Jesse Antoszyk <jcantosz@us.ibm.com>
2017-04-11 15:18:11 -0400

    initial commit
    Signed-off-by: Jesse Antoszyk <jcantosz@us.ibm.com>

---
+++ k8s/kubed.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+bx login -a api.ng.bluemix.net -u $PLUGIN_USER -p $BM_PASS -o $PLUGIN_ORG -s $PLUGIN_SPACE -c $BM_ACCT
+bx cs init
+$(bx cs cluster-config $PLUGIN_CLUSTER | grep export)
+
+kubectl delete --ignore-not-found=true -f $PLUGIN_YAML
+kubectl create -f $PLUGIN_YAML
+
+
