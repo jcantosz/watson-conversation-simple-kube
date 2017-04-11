2ea68a22933264f95328a1500a6538495c8b86c0
Jesse Antoszyk <jcantosz@us.ibm.com>
2017-04-11 15:39:10 -0400

    can we build it?
    Signed-off-by: Jesse Antoszyk <jcantosz@us.ibm.com>

--- k8s/kubed.sh
+++ k8s/kubed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bx login -a api.ng.bluemix.net -u $PLUGIN_USER -p $BM_PASS -o $PLUGIN_ORG -s $PLUGIN_SPACE -c $BM_ACCT
+bx login -a api.ng.bluemix.net -u $PLUGIN_USER -p $BM_PASS -o $PLUGIN_ORG -s $PLUGIN_SPACE -c $BM_ACCT > /dev/null
 bx cs init
 $(bx cs cluster-config $PLUGIN_CLUSTER | grep export)
 
